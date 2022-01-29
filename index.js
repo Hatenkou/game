dc69882d19237eb21153b4212c5fce0d250dc0d3
Vitya <vitya199600@gmail.com>
2022-01-29 10:09:23 +0200

    Player creation

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 const canvas = document.querySelector('canvas');
 const canvasContext = canvas.getContext('2d');
 
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
 console.log(canvasContext);
+
+class Player {
+   constructor() {
+      this.position = {
+         x: 100,
+         y: 100
+      }
+      this.width = 30
+      this.haight = 30
+   }
+
+   draw() {
+      canvasContext.fillRect(this.position.x, this.position.y, this.width, this.haight);
+   }
+}
+
+const player = new Player()
+player.draw()
