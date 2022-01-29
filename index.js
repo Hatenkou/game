2828027560c19b0f9f1936f902164c007e082c14
Vitya <vitya199600@gmail.com>
2022-01-29 10:42:19 +0200

    created gravity for player

--- index.js
+++ index.js
@@ -6,20 +6,44 @@ canvas.height = innerHeight;
 
 console.log(canvasContext);
 
+const gravity = 1.5;
+
 class Player {
    constructor() {
       this.position = {
          x: 100,
          y: 100
       }
+      this.velocity = {
+         x: 0,
+         y: 0
+      }
       this.width = 30
-      this.haight = 30
+      this.height = 30
    }
 
    draw() {
-      canvasContext.fillRect(this.position.x, this.position.y, this.width, this.haight);
+      canvasContext.fillStyle = "green"
+      canvasContext.fillRect(this.position.x, this.position.y, this.width, this.height);
+   }
+
+   update() {
+      this.draw()
+      this.position.y += this.velocity.y
+
+      if (this.position.y + this.height + this.velocity.y <= canvas.height)
+         this.velocity.y += gravity
+      else this.velocity.y = 0
    }
 }
 
 const player = new Player()
-player.draw()
+
+
+function animate() {
+   requestAnimationFrame(animate)
+   canvasContext.clearRect(0, 0, canvas.width, canvas.height)
+   player.update()
+}
+
+animate();
